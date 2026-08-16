726e3a15c8e5ba04a48972a8995a6b0d166d4025
adi.p <adhiesnc@gmail.com>
2026-08-17 01:44:57 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,26 @@
 // =================================================================
-// 🔥 KONFIGURASI DATABASE & GATEWAY PWA PRESENSI UGD
+// 🔥 KONFIGURASI SISTEM PRESENSI UGD PUSKESMAS SAMPANAHAN
 // =================================================================
 const CONFIG = {
-  VERSION: "v1.4.3",
-  DEFAULT_QR: "UGD-SAMPANAHAN-SCAN",
-  MONTHS: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+    VERSION: "v1.4.3",
+    // URL Web App Google Apps Script terbaru
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyKSlGKfAiQX1jWHNrtsDjYF6UXtd2KV9Opz3lVdV8jYxqh_nVc72TJ1oRD1bP8J9iN7w/exec",
+    DEFAULT_QR: "UGD-SAMPANAHAN-SCAN",
+    MONTHS: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+    
+    // Konfigurasi Project Firebase: absensiugd-bd829
+    FIREBASE: {
+        apiKey: "AIzaSyBvEdfaUp4EodR9zSMLGef4VPyU2ZyayLw",
+        authDomain: "absensiugd-bd829.firebaseapp.com",
+        databaseURL: "https://absensiugd-bd829-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "absensiugd-bd829",
+        storageBucket: "absensiugd-bd829.firebasestorage.app",
+        messagingSenderId: "69298444767",
+        appId: "1:69298444767:web:e7c395a3614b9a2ea74c6a",
+        measurementId: "G-JHMPY9S71M"
+    },
 
-  // Konfigurasi Firebase Realtime Database
-  FIREBASE: {
-    apiKey: "AIzaSyBvEdfaUp4EodR9zSMLGef4VPyU2ZyayLw",
-    authDomain: "absensiugd-bd829.firebaseapp.com",
-    databaseURL: "https://absensiugd-bd829-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "absensiugd-bd829",
-    storageBucket: "absensiugd-bd829.firebasestorage.app",
-    messagingSenderId: "69298444767",
-    appId: "1:69298444767:web:e7c395a3614b9a2ea74c6a",
-    measurementId: "G-JHMPY9S71M"
-  },
-
-  // Gateway Notifikasi WhatsApp (Fonnte)
-  FONNTE_TOKEN: "p1NYTiLzm5KFnFAt9uNr",
-  NOMOR_NOTIFICATION_CENTER: "120363427997735746@g.us"
+    // Gateway Notifikasi WhatsApp (Fonnte)
+    FONNTE_TOKEN: "p1NYTiLzm5KFnFAt9uNr",
+    NOMOR_NOTIFICATION_CENTER: "120363427997735746@g.us"
 };
